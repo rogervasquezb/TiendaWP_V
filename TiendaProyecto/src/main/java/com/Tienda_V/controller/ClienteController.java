a6b3c83c6cfe52d1d66a66f2d1890385602d2a4c
rogervasquezb <rvasquez0129@gmail.com>
2022-08-10 08:54:12 -0600

    Tarea 04 Reportes Hecha

--- TiendaProyecto/src/main/java/com/Tienda_V/controller/ClienteController.java
+++ TiendaProyecto/src/main/java/com/Tienda_V/controller/ClienteController.java
@@ -7,7 +7,13 @@ package com.Tienda_V.controller;
 
 import com.Tienda_V.domain.Cliente;
 import com.Tienda_V.service.ClienteService;
+import com.Tienda_V.service.ClienteReportService;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -18,43 +24,69 @@ import org.springframework.web.bind.annotation.*;
  */
 @Controller
 public class ClienteController {
+
     @Autowired
     private ClienteService clienteService;
+    @Autowired
+    private ClienteReportService clienteReportService;
+
     @GetMapping("/cliente/listado")
     public String inicio(Model model) {
-        var clientes=clienteService.getClientes();
-        
-        var limiteTotal=0;
-        for (var c: clientes) {
-            limiteTotal+=c.credito.limite;
+        var clientes = clienteService.getClientes();
+
+        var limiteTotal = 0;
+        for (var c : clientes) {
+            limiteTotal += c.credito.limite;
         }
-        model.addAttribute("limiteTotal",limiteTotal);
-        model.addAttribute("totalClientes",clientes.size());
-        
-        model.addAttribute("clientes",clientes);
+        model.addAttribute("limiteTotal", limiteTotal);
+        model.addAttribute("totalClientes", clientes.size());
+
+        model.addAttribute("clientes", clientes);
         return "/cliente/listado";
     }
+
     @GetMapping("/cliente/nuevo")
-    public String nuevoCliente(Cliente cliente){
+    public String nuevoCliente(Cliente cliente) {
         return "/cliente/modificar";
     }
+
     @PostMapping("/cliente/guardar")
-    public String guardarCliente(Cliente cliente){
+    public String guardarCliente(Cliente cliente) {
         clienteService.save(cliente);
         return "redirect:/cliente/listado";
     }
+
     @GetMapping("/cliente/modificar/{idCliente}")
-    public String modificarCliente(Cliente cliente, Model model){
+    public String modificarCliente(Cliente cliente, Model model) {
         cliente = clienteService.getCliente(cliente);
         model.addAttribute("cliente", cliente);
         return "/cliente/modificar";
-        
+
     }
+
     @GetMapping("/cliente/eliminar/{idCliente}")
-    public String eliminarCliente(Cliente cliente, Model model){
+    public String eliminarCliente(Cliente cliente, Model model) {
         clienteService.delete(cliente);
         return "redirect:/cliente/listado";
-        
+
     }
-    
+
+    @GetMapping(value = "/cliente/ReporteClientes", produces = MediaType.APPLICATION_PDF_VALUE)
+    public @ResponseBody
+    byte[] getFile() throws IOException {
+        try {
+            FileInputStream fis = new FileInputStream(new File(clienteReportService.generateReport()));
+            byte[] targetArray = new byte[fis.available()];
+            fis.read(targetArray);
+            return targetArray;
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
---
+++ TiendaProyecto/src/main/java/com/Tienda_V/service/ClienteReportService.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Tienda_V.service;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.HashMap;
+import java.util.Map;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
+
+/**
+ *
+ * @author rvasq
+ */
+@Service
+public class ClienteReportService {
+
+    private String reportPath;
+
+    public String generateReport() {
+        try {
+            File file = ResourceUtils.getFile("classpath:ReporteCliente.jasper");
+            reportPath = file.getParent();
+// Carga el reporte
+            JasperReport jasperReport = (JasperReport) JRLoader.loadObject(file);
+// Añade paramáteros (pero para este caso no hay)
+            Map<String, Object> parameters = new HashMap<>();
+//Instanciar la conexión
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/techshop", "usuario_prueba", "Usuar1o_Clave.");
+// Llena el reporte
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, con);
+// Exporta a PDF
+            JasperExportManager.exportReportToPdfFile(jasperPrint, reportPath + "\\ReporteClientes.pdf");
+            return reportPath + "\\ReporteClientes.pdf";
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+}
